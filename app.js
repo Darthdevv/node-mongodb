7ce55ce953a80c7238bfc8a6d411afce797cd57f
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-06-15 23:10:43 +0300

    🍃 chore: connecting to mongodb server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import { dbConnection } from './db/connection.js';
 
 const app = express();
 
 app.use(express.json());
+
+dbConnection();
 
 const PORT = 8000;
 
---
+++ db/connection.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'carRental';
+
+export async function dbConnection() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to server");
+  } catch (error) {
+    console.log("Error connecting to server", error);
+  }
+}
