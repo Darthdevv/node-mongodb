c0e0d88dc205001b5f9b5dc94e51eb3c4732a814
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-06-16 23:51:38 +0300

    🍃 feat: settig up database connection

--- db/connection.js
+++ db/connection.js
@@ -6,12 +6,17 @@ const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'carRental';
+let db;
 
 export async function dbConnection() {
   try {
     await client.connect();
     console.log("Connected successfully to server");
+    db = client.db(dbName);
+    const Users = db.collection("users");
   } catch (error) {
     console.log("Error connecting to server", error);
   }
 }
+
+export { db };
