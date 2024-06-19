db417e1cfd76b935e7a20f2e36d6f29b75ecc58f
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-06-19 19:06:57 +0300

    🍃 feat: adding cars, getting all cars amd getting a specific car

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,10 +1,57 @@
+import Car from "../models/car.model.js";
 
+export const createCar = async (req, res) => {
+  try {
+    const { name, model, rentalStatus } = req.body;
 
-export const createCar = async (req, res) => {};
+    if (!name || !model || !rentalStatus) {
+      return res.status(400).jsonn("Please fill all fields.");
+    }
 
-export const retreiveCars = async (req, res) => {};
+    const newCar = await Car.create({
+      name,
+      model,
+      rentalStatus,
+    });
 
-export const retreiveCar = async (req, res) => {};
+    if (!newCar) {
+      return res.status(400).json("failed to add this car");
+    }
+
+    res.status(201).json({ car: newCar });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const retreiveCars = async (req, res) => {
+    try {
+    const cars = await Car.find();
+
+    if (!cars) {
+      return res.status(404).json({ message: "No cars found" });
+    }
+
+    res.status(200).json({ cars });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const retreiveCar = async (req, res) => {
+  try {
+    const { _id: id } = req.params;
+    const car = await Car.findOne({ id });
+
+    if (!car) {
+      return res.status(404).json({ message: "Car not found" });
+    }
+
+    res.status(200).json({ car });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 export const updateCar = async (req, res) => {};
 
