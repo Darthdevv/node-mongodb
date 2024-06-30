9fd452869c6900c0d076ab69e615db00baf438c7
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-06-30 23:20:48 +0300

    🍃 chore: setting up api for deployment on server

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ import { errorHandler } from './middlewares/error.middleware.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-dotenv.config();
 app.use(cors());
 
 app.use(express.json());
