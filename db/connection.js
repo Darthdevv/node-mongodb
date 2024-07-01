ccfd2000d0265e830762ae7f587cb6b5ca59c7e5
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-01 04:14:50 +0300

    🍃 chore: setting up api for deployment on server

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
+    await mongoose.connect(process.env.MONGO_DB_URI),
+      {
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      };
     console.log("connected to MongoDB sucessfully ✅");
   } catch (error) {
     console.log(`Failed to connect to Mongo: ${error.message} ❌`);
